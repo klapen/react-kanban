1cab6cd970fbdc836536a9ae5ac36e3c2c1b5b21
klapen <oasaavedrac@gmail.com>
2018-01-25 14:42:01 -0500

    Add delete button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,26 +2,38 @@ import React from 'react';
 import Notes from './Notes';
 import uuid from 'uuid';
 
-export default class App extends React.Component{
+const notes = [
+    {id: uuid.v4(),task: 'Learn React'},
+    {id: uuid.v4(),task: 'Do laundry'}
+];
+
+const new_note = [
+    {id: uuid.v4(), task: 'New task'}
+];
+
+class App extends React.Component{
     constructor(props){
         super(props);
 	
-        this.state = {notes:[{id: uuid.v4(),task: 'Learn React'},{id: uuid.v4(),task: 'Do laundry'}]};
+        this.state = {notes:notes}
     }
     render() {
         const {notes} = this.state;
         return (
             <div>
               <button onClick={this.addNote}>+</button>
-              <Notes notes={notes}/>
+              <Notes notes={notes} onDelete={this.deleteNote}/>
             </div>
         );
     }
     addNote = () => {
-        this.setState({
-            notes: this.state.notes.concat([
-                {id: uuid.v4(), task: 'New task'}
-            ])
-        });
+        this.setState({notes: this.state.notes.concat(new_note)});
+    }
+    deleteNote = (id,e) => {
+	e.stopPropagation();
+
+	this.setState({notes: this.state.notes.filter(note => note.id !== id)});
     }
 }
+
+export default App;
